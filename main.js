514993d99774ca92cdc39ea35ffcc2bd48051f3a
Alexaloka <103824973+Alexaloka@users.noreply.github.com>
2023-03-20 14:29:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -29,3 +29,52 @@ function speak()
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis);
 }
+
+function check()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+
+function gotResult(error,results)
+{
+    if(error){
+        console.error(error);
+    }else {
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction1=results[0].label;
+        prediction2=results[1].label;
+        speak();
+        if(results[0].label == "happy")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128512;";
+        }
+        if(results[0].label == "sad")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128530;";
+        }
+        if(results[0].label == "angry")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128530;";
+        }
+        if(results[1].label == "happy")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128512;";
+        }
+        if(results[1].label == "sad")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128530;";
+        }
+        if(results[1].label == "angry")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128530;";
+        }
+
+    }
+
+    
+
+
+}
